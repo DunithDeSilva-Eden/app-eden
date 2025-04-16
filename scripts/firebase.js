157659edb2351ca9cdcc36fae256247fbc3f8abd
DunithDeSilva-Eden <dunithdesilva@edeninvestments.xyz>
2025-04-16 13:41:12 +0800

    Create firebase.js

---
+++ scripts/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBwaa9ySSIS7Zh6ZRJ0iuhpRhi2QPgL9Hg",
+  authDomain: "app-eden-a8458.firebaseapp.com",
+  projectId: "app-eden-a8458",
+  storageBucket: "app-eden-a8458.firebasestorage.app",
+  messagingSenderId: "852310432893",
+  appId: "1:852310432893:web:594ecda49b3f979c7ae514",
+  measurementId: "G-TZ6VYSH3R8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
